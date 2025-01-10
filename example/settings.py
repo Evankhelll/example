be2f7ba36cd23aba896fa76a564453dd7895fe73
Raka Aka <50712428+Oraicon@users.noreply.github.com>
2025-01-10 23:17:25 +0700

    ~

--- example/settings.py
+++ example/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres.thxgkajktztcqshlmioc",
         "PASSWORD": "P4ssw0rd.2019",
