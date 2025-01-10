953b2962ffed97b0f58f9abe369071b3f6196031
Raka Aka <50712428+Oraicon@users.noreply.github.com>
2025-01-10 23:48:34 +0700

    ~

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,6 @@ router.register("komentar", api.komentarViewSet)
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),  # Must use a list
-    path('dominant-colors/<int:id>/', views.get_colors_as_json, name='dominant_colors'),
+    # path('dominant-colors/<int:id>/', views.get_colors_as_json, name='dominant_colors'),
     path('dominant-colors2/<int:id>/', views.get_colors_as_json2, name='dominant_colors2'),
 ]
